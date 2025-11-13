4b6e073f76d53c244cf9e8ab3301ddba6be1451f
Raul Crisostomo Gomes <raul.cgomes92@gmail.com>
2025-11-13 04:47:58 +0000

    add date time

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 function Home(){
-    return <h1>Clau esse link funciona?</h1>
+    return <h1>Clau esse link funciona? E isso está certo? ${getDateTime}</h1>
+}
+
+function getDateTime(){
+    const now = new Date();
+    const hour = now.getHours();
+    const minute = now.getMinutes();
+    return `${hour}:${minute}`;
 }
 
 export default Home;
