39d09bbed4cad4063b5a747ff1d53df3ee737d12
Raul Crisostomo Gomes <raul.cgomes92@gmail.com>
2025-11-13 04:53:39 +0000

    change sequence

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-function Home(){
-    return <h1>Clau esse link funciona? E isso está certo? ${getDateTime}</h1>
-}
-
 function getDateTime(){
     const now = new Date();
     const hour = now.getHours();
     const minute = now.getMinutes();
     return `${hour}:${minute}`;
+}
+
+function Home(){
+    return <h1>Clau esse link funciona? E isso está certo? {getDateTime}</h1>
 }
 
 export default Home;
